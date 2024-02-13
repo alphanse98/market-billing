2835be82a2e23b552bca6723c4bee15974eed58b
alphanse98 <alphanse98@gmail.com>
2024-02-13 16:22:13 +0530

    ui fix

--- src/components/Custom/Itemlisting.tsx
+++ src/components/Custom/Itemlisting.tsx
@@ -1,10 +1,10 @@
 const Itemlisting = () => {
   return (
     <>
-      <div>
-        <div className="w-36 p-3 shadow-lg rounded-sm border border-stroke bg-white  shadow-default dark:border-strokedark dark:bg-boxdark">
+      {/* <div> */}
+        <div className="w-36 p-3 rounded-lg cursor-pointer shadow-lg rounded-sm border border-stroke bg-white  shadow-default dark:border-strokedark dark:bg-boxdark">
           <div className="px-1">
-            <div className="mb-1 font-bold text-lg font-medium text-black dark:text-white">Product Title</div>
+            <div className="mb-1 font-bold text-lg font-medium text-black dark:text-white">Product Title test test</div>
           </div>
           <div className="px-1">
             <span className="inline-block bg-gray-200 rounded-full text-sm font-semibold text-gray-700">
@@ -12,7 +12,7 @@ const Itemlisting = () => {
             </span>
           </div>
         </div>
-      </div>
+      {/* </div> */}
     </>
   );
 };
