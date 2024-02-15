354d6067a1a836a492ce3bd5a1a29b01c63b404d
R.soorya <sooryayaron147@gmail.com>
2024-02-15 11:33:34 +0530

    customer-page-ui

--- src/CustomComponents/CustomerList.tsx
+++ src/CustomComponents/CustomerList.tsx
@@ -1,6 +1,12 @@
 
+import DeleteIcon from "../Assets/SvgIcons/DeleteIcon"
+import DownloadIcon from "../Assets/SvgIcons/DownloadIcon"
+import EyeIcon from "../Assets/SvgIcons/EyeIcon"
+import { CustomerList } from "../JasonMockData/CustomersData"
 
-const CustomerList = ()=>{
+
+
+const CustomerListTable = ()=>{
     return(
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
         <div className="py-6 px-4 md:px-6 xl:px-7.5">
@@ -13,36 +19,39 @@ const CustomerList = ()=>{
           <div className="col-span-1 flex items-center">
             <p className="font-medium">Id</p>
           </div>
-          <div className="col-span-3 hidden items-center sm:flex">
-            <p className="font-medium">Item Name</p>
+          <div className="col-span-2 hidden items-center sm:flex">
+            <p className="font-medium">Customer Name</p>
           </div>
           <div className="col-span-2 flex items-center">
-            <p className="font-medium">Price</p>
+            <p className="font-medium">Customer Email</p>
           </div>
           <div className="col-span-2 flex items-center">
+            <p className="font-medium">Customer PhoneNumber</p>
+          </div>
+          <div className="col-span-1 flex items-center">
             <p className="font-medium">Action</p>
           </div>
         </div>
   {
-   ItemsMockData && ItemsMockData.map((item:any)=>(
+   CustomerList && CustomerList.map((item:any)=>(
       <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
        <div className="col-span-1 hidden items-center  sm:flex">
         <p className="text-sm text-black dark:text-white">{item.Id}</p>
       </div>
-      <div className="col-span-3 flex items-center">
+      <div className="col-span-2 flex items-center">
         <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-          <div className="h-12.5 w-15 rounded-md ">
-            <img src={item.ItemImg} alt="Product" />
-          </div>
           <p className="text-sm text-black dark:text-white">
-            {item.ItemName}
+            {item.CustomerName}
           </p>
         </div>
       </div>
       <div className="col-span-2 flex items-center justify-start">
-        <p className="text-sm text-black  dark:text-white">{item.ItemPrice}</p>
+        <p className="text-sm text-black  dark:text-white">{item.CustomerEmail}</p>
       </div>
-      <div className="col-span-2 flex items-center">
+      <div className="col-span-2 flex items-center justify-start">
+        <p className="text-sm text-black  dark:text-white">{item.CustomerNumber}</p>
+      </div>
+      <div className="col-span-1 flex items-center">
       <div className="flex items-center space-x-3.5">
                     <button className="hover:text-primary">
                       <EyeIcon/>
@@ -63,4 +72,4 @@ const CustomerList = ()=>{
     )
 }
 
-export default CustomerList
+export default CustomerListTable
