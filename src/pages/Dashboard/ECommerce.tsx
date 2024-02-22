d50775acd37c97dea965899681de5beb09ea4256
R.soorya <sooryayaron147@gmail.com>
2024-02-22 14:01:01 +0530

    print-ui

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -8,8 +8,20 @@ import ChartTwo from '../../components/ChartTwo.tsx';
 import ChatCard from '../../components/ChatCard.tsx';
 import MapOne from '../../components/MapOne.tsx';
 import TableOne from '../../components/TableOne.tsx';
+import React,{useRef} from "react"
+import {useReactToPrint} from "react-to-print"
+import PrintBillUi from "../../CustomComponents/PrintBillUi"
+
 
 const ECommerce = () => {
+  const componentRef = useRef()
+  const handlePrint = useReactToPrint({
+    content: ()=>componentRef.current
+  })
+
+//  const CardFour = React.forwardRef((props, ref) => {
+//     return <div ref={ref}><CardFour/></div>;
+//   });
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
@@ -19,6 +31,16 @@ const ECommerce = () => {
         <CardFour />
       </div>
 
+      <div>
+        {/* <CardFour ref={componentRef} /> */}
+        <div ref={componentRef}>
+           <div>
+           <PrintBillUi/>
+           </div>
+        </div>
+      <button onClick={()=>handlePrint()} className="bg-warning m-1 p-1 text-black rounded-md">print</button>
+
+      </div>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
